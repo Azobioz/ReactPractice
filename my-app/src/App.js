27c6a29573ef7d541a14dcdb52f514e6a85d2c05
Azobioz <aziz345568667@gmail.com>
2024-11-02 01:45:18 +0300

    commit
    хуки, useState, defaultProps,  componentDidUpdate

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,17 +14,25 @@ class App extends React.Component {
 
         this.inputClick = this.inputClick.bind(this)
 
+
+    }
+
+    componentDidUpdate(previousProp) {
+        if(this.state.helpText !== "Help") {
+            console.log("Some text")
+        }
     }
 
 
     render() {
         return <div className="name">
-            <Header title="Шапка" name="123"/>
+            <Header title="Шапка"/>
             <Header title="Сайта"/>
             <h1>{this.state.helpText}</h1>
             <h2>{this.state.userData}</h2>
             <input placeholder={this.state.helpText}
-                   onChange={event=> this.setState({userData: event.target.value})}
+                   onChange={event=> this.setState({userData: event.target.value,
+                    helpText: event.target.value} )}
                    onClick={this.inputClick} onMouseOver={this.mouseOver}/>
             <p>{this.state.helpText === "Help text" ? "Yes" : "No"}</p>
         <Image image={reactImage} />
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,9 +1,15 @@
 import React from "react"
+import Button from "./Button";
 
 class Header extends React.Component {
     render () {
-        return (<header className="header">{this.props.title} {this.props.name}</header>)
+        return (<header className="header">
+            {this.props.title}
+            <Button/>
+            <Button text="Button"/>
+        </header>)
     }
+
 }
 
 export default Header
